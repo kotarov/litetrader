4776b91a24801141ee254481f8fe0c27751345bb
skotarov <skotarov@gmail.com>
2016-05-20 09:59:08 +0000

    mmm

--- ajaxAdmin/tools/update.php
+++ ajaxAdmin/tools/update.php
@@ -2,6 +2,6 @@
 $ret = array();
 
 $ret['success'] = shell_exec('git pull');
-$ret['data']['ver'] = file_get_contents("../ver");
+$ret['data']['ver'] = file_get_contents("../../ver");
 
 return json_encode($ret);
