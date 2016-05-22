fd985556532a6f2a87a831ddc5a660f4cb38d020
skotarov <skotarov@gmail.com>
2016-05-22 07:06:55 +0000

    m

---
+++ public/cart/modal.php
@@ -0,0 +1,13 @@
+<style>
+            #modal-cart .quantity { width:4em};
+        </style>
+        <div id="modal-cart" class="uk-modal">
+            <div class="uk-modal-dialog"><a class="uk-modal-close uk-close"></a>
+                <div class="uk-modal-header"><h3>Yours shopping cart</h3></div>
+                <?php include __DIR__."/../cart/content.php";?>
+                <div class="uk-modal-footer">
+                    <button class="uk-button uk-modal-close ">Close</button>
+                    <a href="<?=URL_BASE?>order/" class="uk-button uk-button-primary uk-button-large uk-float-right"> Checkout <i class="uk-icon-truck"></i></a>
+                </div>
+            </div>
+        </div>
